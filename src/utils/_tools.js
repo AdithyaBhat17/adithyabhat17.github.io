8964d50c203cc8c622623cbb51d9bbe992d8adf5
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-04-14 22:07:19 +0530

    :tada: Dummy db for tools I use

---
+++ src/utils/_tools.js
@@ -0,0 +1,156 @@
+// <!-- ### Devices
+
+// * [Asus RoG Strix 15](https://www.amazon.in/15-6-inch-Laptop-Windows-Graphics-GL503GE-EN041T/dp/B07DQPFD21/)
+// * [Apple iPad 6th Gen (2018)](https://www.amazon.in/Apple-iPad-Tablet-Wi-Fi-Space/dp/B07C4YKR3J/)
+// * [Apple Pencil - 1st Gen](https://www.amazon.in/Apple-MK0C2ZM-Pencil-iPad-White/dp/B016NY7784/)
+
+// ### Design
+
+// * [ProCreate](https://procreate.art/)
+// * [Adobe Illustrator](https://www.adobe.com/in/products/illustrator.html)
+// * [Figma](https://figma.com)
+// * [Adobe XD](https://www.adobe.com/in/products/xd.html)
+
+// ### Development
+
+// * [VS Code Editor](https://code.visualstudio.com)
+// * [Hyper Terminal](https://hyper.is)
+// * [Chrome Canary](https://www.google.com/intl/en_in/chrome/canary/)
+// * [Postman](https://www.getpostman.com/)
+// * [Git and Github](https://github.com)
+
+// ### Operating System
+
+// * [Windows 10](https://www.microsoft.com/en-in/software-download/windows10)
+// * [KDE Neon](https://neon.kde.org/)
+
+// ### Notes
+
+// * [Evernote](https://evernote.com)
+// * [OneNote](https://onenote.com)
+// * [Notion](https://notion.so)
+
+// ### Task Management
+
+// * [Todoist](https://todoist.com/?lang=en)
+// * [Any.do](https://)
+//  -->
+
+export const tools = [{
+    "Devices": [{
+        name: 'Asus RoG Strix 15',
+        link: 'https://www.amazon.in/15-6-inch-Laptop-Windows-Graphics-GL503GE-EN041T/dp/B07DQPFD21/'
+    },
+    {
+        name: 'Apple iPad 6th Gen (2018)',
+        link: 'https://www.amazon.in/Apple-iPad-Tablet-Wi-Fi-Space/dp/B07C4YKR3J/'
+    },
+    {
+        name: 'Apple Pencil - 1st Gen',
+        link: 'https://www.amazon.in/Apple-MK0C2ZM-Pencil-iPad-White/dp/B016NY7784/'
+    }],
+    "Design": [{
+        name: 'ProCreate',
+        link: 'https://procreate.art/'
+    },
+    {
+        name: 'Adobe Illustrator',
+        link: 'https://www.adobe.com/in/products/illustrator.html'
+    },
+    {
+        name: 'Figma',
+        link: 'https://figma.com'
+    }],
+    "Development": [{
+        name: 'VS Code Editor',
+        link: 'https://code.visualstudio.com'
+    },
+    {
+        name: 'Hyper Terminal',
+        link: 'https://hyper.is'
+    },
+    {
+        name: 'Chrome Canary',
+        link: 'https://www.google.com/intl/en_in/chrome/canary/'
+    },
+    {
+        name: 'Postman',
+        link: 'https://www.getpostman.com/'
+    },
+    {
+        name: 'Git and Github',
+        link: 'https://www.github.com/'
+    }],
+    "OS": [{
+        name: 'Windows 10 Home',
+        link: 'https://www.microsoft.com/en-in/software-download/windows10'
+    },
+    {
+        name: 'KDE Neon',
+        link: 'https://neon.kde.org/'
+    }],
+    "Notes": [{
+        name: 'Evernote',
+        link: 'https://evernote.com'
+    },
+    {
+        name: 'OneNote',
+        link: 'https://onenote.com'
+    },
+    {
+        name: 'Notion',
+        link: 'https://notion.so'
+    }],
+    "Task Management": [{
+        name: 'Trello',
+        link: 'https://trello.com'
+    },
+    {
+        name: 'Todoist',
+        link: 'https://todoist.com/?lang=en'
+    },
+    {
+        name: 'Any.do',
+        link: 'https://any.do'
+    }],
+    "Blogs": [{
+        name: 'The Dev Community',
+        link: 'https://dev.to'
+    },
+    {
+        name: 'Medium',
+        link: 'https://medium.com'
+    },
+    {
+        name: 'Overreacted by Dan Abramov',
+        link: 'https://overreacted.io'
+    },
+    {
+        name: 'UX Collective',
+        link: 'https://uxdesign.cc/'
+    },
+    {
+        name: 'Robin Wieruch',
+        link: 'https://www.robinwieruch.de/'
+    }],
+    "Courses": [{
+        name: 'Interaction Design Foundation',
+        link: 'https://interaction-design.org'
+    },
+    {
+        name: 'The Web Developer Bootcamp',
+        link: 'https://www.udemy.com/the-web-developer-bootcamp/'
+    },
+    {
+        name: 'The Web Developer Bootcamp',
+        link: 'https://www.udemy.com/the-web-developer-bootcamp/'
+    },
+    {
+        name: 'React Nanodegree',
+        link: 'https://in.udacity.com/course/react-nanodegree--nd019' 
+    },
+    {
+        name: 'Python For Everybody',
+        link: 'https://www.coursera.org/specializations/python' 
+    }]
+}]
